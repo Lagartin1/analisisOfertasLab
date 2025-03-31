5653fc44ba70671963c1f85ef8503157943ecdc9
Rodolfo Seguel <rodolfo.seguel@alumnos.uach.cl>
2025-03-31 19:34:24 -0300

    config.sh arreglado

--- config.sh
+++ config.sh
@@ -1,2 +1,28 @@
-echo "creando archivo de .env"
-echo "LINK_FILE_1=\nCSV_CHILETRABAJOS=\nCSV_COMPUTRABAJO=" > .env"
+#!/bin/bash
+
+# Name of the .env file
+env_file=".env"
+
+# Check if the .env file already exists
+if [ -f "$env_file" ]; then
+    echo "The $env_file file already exists. Do you want to overwrite it? (y/n)"
+    read response
+    if [ "$response" != "y" ]; then
+        echo "The file has not been overwritten."
+        exit 1
+    fi
+fi
+
+# Create the .env file with basic variables
+echo "Creating the .env file..."
+
+cat <<EOL > "$env_file"
+# Config file
+
+LINK_FILE_1=
+CSV_CHILETRABAJOS=
+CSV_COMPUTRABAJO=
+
+EOL
+
+echo "The .env file has been created successfully."
